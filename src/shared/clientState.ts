99b78ee0872d7b930229a8e023d644b64804a2f1
zkzkatz <mandrillx@protonmail.com>
2023-12-13 22:15:04 -0500

    remove: default systems
    replaced with default project systems

--- src/shared/clientState.ts
+++ src/shared/clientState.ts
@@ -1,5 +1,9 @@
+import { AnyEntity } from "@rbxts/matter";
+
 export interface ClientState extends BaseState {
 	character: BaseCharacter;
+	playerId: AnyEntity | undefined;
+	entityIdMap: Map<string, AnyEntity>;
 }
 
 export interface BaseState {
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,5 +1,2 @@
-export * from "./baseComponents";
 export * from "./clientState";
-export * from "./start";
 export * from "./util";
-export * from "./setupTags";
